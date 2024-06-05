ab0358287dc2f27c1a9ea3e52a70912fd68bd7e1
fykaa <faeka6@gmail.com>
2024-06-06 03:17:27 +0530

    sitedata: include go package html/template
    Signed-off-by: fykaa <faeka6@gmail.com>

--- main.go
+++ main.go
@@ -4,17 +4,47 @@ import (
 	"fmt"
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/mem"
+	"html/template"
+	"log"
 	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+type monitor struct {
+	CpuPercent []float64
+	MemPercent *mem.VirtualMemoryStat
+	Message    string
+}
+
+var data monitor
+
 func index(w http.ResponseWriter, r *http.Request) {
-	cpuPercent, _ := cpu.Percent(0, false)
-	memPercent, _ := mem.VirtualMemory()
-	message := ""
-	if cpuPercent[0] > 2 || memPercent.UsedPercent > 10 {
-		message = "High CPU or Memory utilization. Please scale up!!"
+
+	data.CpuPercent, _ = cpu.Percent(0, false)
+	data.MemPercent, _ = mem.VirtualMemory()
+	data.Message = ""
+
+	if data.CpuPercent[0] > 50 && data.MemPercent.UsedPercent > 50 {
+		data.Message = "Warning: High CPU and Memory usage detected. Optimize your application and scale resources if needed."
+	} else if data.CpuPercent[0] > 50 {
+		data.Message = "Warning: High CPU usage detected. Optimize your application for better performance."
+	} else if data.MemPercent.UsedPercent > 50 {
+		data.Message = "Warning: High Memory usage detected. Consider scaling your resources."
+	} else {
+		data.Message = "CPU and Memory usage are within acceptable limits. Keep up the good work!"
 	}
-	fmt.Fprintf(w, "CPU util: %.2f%% and memory utl: %.2f%%\n%s", cpuPercent[0], memPercent.UsedPercent, message)
+
+	err := tpl.ExecuteTemplate(w, "index.gohtml", data)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//fmt.Fprintf(w, "CPU util: %.2f%% and memory utl: %.2f%%\n%s", data.cpuPercent[0], data.memPercent.UsedPercent, message)
 }
 
 func main() {
